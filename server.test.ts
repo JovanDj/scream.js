8f219e6f96ec5bb5adcf4d676edaa39b5350e0bb
Jovan <JovanDj@users.noreply.github.com>
2021-05-14 14:10:41 +0200

    feat: add cross-env

--- server.test.ts
+++ server.test.ts
@@ -11,12 +11,6 @@ describe("server", () => {
     server.close();
   });
 
-  it("shoud return response", async (done) => {
-    const response = await request(server).get("/");
-    expect(response.text).toBe("test");
-    done();
-  });
-
   it("should get users", async (done) => {
     const response = await request(server).get("/users");
     expect(response.text).toBe("GET /users");
