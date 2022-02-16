c5baf4cb7cf7eb8bd51dd2fea6d6e98fd60bdc07
Jovan <JovanDj@users.noreply.github.com>
2022-02-16 22:40:40 +0100

    Fix router

--- lib/http/http-context.ts
+++ lib/http/http-context.ts
@@ -4,17 +4,7 @@ export class HTTPContext {
   constructor(
     private readonly _request: IncomingMessage,
     private readonly _response: ServerResponse
-  ) {
-    this.request.on("error", (err) => {
-      console.error(err);
-      this.response.statusCode = 400;
-      this.response.end();
-    });
-
-    this.response.on("error", (err) => {
-      console.error(err);
-    });
-  }
+  ) {}
 
   get request() {
     return this._request;
