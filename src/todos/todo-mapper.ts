df8c7c752df12ea99fd2e6624f74a839a48af5d5
Jovan <JovanDj@users.noreply.github.com>
2022-02-28 14:43:39 +0100

    Table gateway

--- src/todos/todo-mapper.ts
+++ src/todos/todo-mapper.ts
@@ -1,19 +1,11 @@
-import { Database } from "sqlite";
 import { Todo } from "./todo";
+import { TodoGateway } from "./todo-gateway";
 
 export class TodoMapper {
-  private readonly _table = "todos";
+  constructor(private readonly gateway: TodoGateway) {}
 
-  constructor(private readonly db: Database) {}
-
-  async findById(id: number): Promise<Todo> {
-    const query = "SELECT * FROM todos WHERE id = $id";
-    const preparedStatement = await this.db.prepare(query);
-
-    await preparedStatement.bind({ $id: id });
-    const result = await preparedStatement.get();
-
-    await preparedStatement.finalize();
+  async findById(id: number): Promise<Todo | undefined> {
+    const result = await this.gateway.findById(id);
 
     const todo = new Todo();
 
@@ -24,12 +16,7 @@ export class TodoMapper {
   }
 
   async first(): Promise<Todo> {
-    const query = `SELECT * FROM ${this._table} ORDER BY ROWID ASC LIMIT 1`;
-    const preparedStatement = await this.db.prepare(query);
-
-    const result = await preparedStatement.get();
-
-    await preparedStatement.finalize();
+    const result = await this.gateway.first();
 
     const todo = new Todo();
 
@@ -40,12 +27,7 @@ export class TodoMapper {
   }
 
   async last(): Promise<Todo> {
-    const query = `SELECT * FROM ${this._table} ORDER BY ROWID DESC LIMIT 1;`;
-    const preparedStatement = await this.db.prepare(query);
-
-    const result = await preparedStatement.get();
-
-    await preparedStatement.finalize();
+    const result = await this.gateway.last();
 
     const todo = new Todo();
 
@@ -56,60 +38,26 @@ export class TodoMapper {
   }
 
   async findAll(): Promise<Todo[]> {
-    const query = `SELECT * FROM ${this._table}`;
-    const preparedStatement = await this.db.prepare(query);
-    const results = await preparedStatement.all();
-    await preparedStatement.finalize();
+    const results = await this.gateway.findAll();
 
-    const todos: Todo[] = [];
-    for (const result of results) {
+    return results.map(result => {
       const todo = new Todo();
 
       todo.id = result.id;
       todo.title = result.title;
-      todos.push(todo);
-    }
-
-    return todos;
+      return todo;
+    });
   }
 
   async insert({ title }: { title: Todo["title"] }): Promise<Todo> {
-    const query = `INSERT INTO ${this._table} (title) VALUES($title)`;
-    const preparedStatement = await this.db.prepare(query);
-    await preparedStatement.bind({ $title: title });
-
-    const { lastID = 0 } = await preparedStatement.run();
-    await preparedStatement.finalize();
-
-    return this.findById(lastID);
+    return this.findById(await this.gateway.insert({ title }));
   }
 
   async update(id: Todo["id"], todo: Partial<Todo>): Promise<number> {
-    let set: string[] = [];
-
-    for (const key of Object.keys(todo)) {
-      set = [...set, `${key} = ?`];
-    }
-
-    const query = `UPDATE ${this._table} SET ${set.join(", ")} WHERE id = ?`;
-    console.log({ query });
-    const preparedStatement = await this.db.prepare(query);
-    await preparedStatement.bind([...Object.values(todo), id]);
-
-    const { changes = 0 } = await preparedStatement.run();
-    await preparedStatement.finalize();
-
-    return changes;
+    return this.gateway.update(id, todo);
   }
 
   async delete(id: Todo["id"]): Promise<number> {
-    const query = `DELETE FROM ${this._table} WHERE id = $id`;
-    const preparedStatement = await this.db.prepare(query);
-    await preparedStatement.bind({ $id: id });
-
-    const { changes = 0 } = await preparedStatement.run();
-    await preparedStatement.finalize();
-
-    return changes;
+    return this.gateway.delete(id);
   }
 }
