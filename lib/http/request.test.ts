4f950c211291ad5eab517dc1cf8414f06c1a5486
Jovan <jovandjukic2@gmail.com>
2023-05-17 19:05:40 +0200

    Finish express facade

---
+++ lib/http/request.test.ts
@@ -0,0 +1,37 @@
+import { beforeEach, describe, expect, it, vi, afterEach } from "vitest";
+
+import { mock, when, instance, reset } from "ts-mockito";
+import { IncomingMessage } from "node:http";
+import { Request } from "./request.js";
+
+describe("Request", () => {
+  let request: Request;
+  let incomingMessage: ReturnType<typeof mock<IncomingMessage>>;
+
+  beforeEach(() => {
+    incomingMessage = mock(IncomingMessage);
+    when(incomingMessage.url).thenReturn("/");
+
+    request = new Request(instance(incomingMessage));
+  });
+
+  it("should be defined", () => {
+    expect(request).toBeDefined();
+  });
+
+  describe("method", () => {
+    it.each([["GET"], ["POST"], ["PATCH"], ["PUT"], ["DELETE"]])(
+      "should return %s",
+      (method) => {
+        when(incomingMessage.method).thenReturn(method);
+        expect(request.method).toStrictEqual(method);
+      }
+    );
+  });
+
+  describe("url", () => {
+    it("should return url", () => {
+      expect(request.url).toStrictEqual("/");
+    });
+  });
+});
--- lib/router/create-express-router.ts
+++ lib/router/create-express-router.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import { ExpressRouter } from "./express-router";
 
 export function createExpressRouter(
   options: Parameters<typeof express.Router>[0]
 ) {
-  return new ExpressRouter(express.Router(options));
+  return express.Router(options);
 }
