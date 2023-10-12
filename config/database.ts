0a2d3158adcad1226121eae5366e1d7ee10ad03c
Jovan <jovandjukic2@gmail.com>
2023-10-12 22:01:55 +0200

    Performance and optimization

--- config/database.ts
+++ config/database.ts
@@ -1,8 +1,6 @@
 import { Database } from "../lib/database/database.js";
 import { SqliteDatabase } from "../lib/database/sqlite.js";
 
-await using db: Database = await new SqliteDatabase(
+export const db: Database = await new SqliteDatabase(
   "migration-test.sqlite",
 ).connect();
-
-export { db };
