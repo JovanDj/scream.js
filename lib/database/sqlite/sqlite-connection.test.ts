000f8832477ce45984374c2ce3875ba9fe12828c
Jovan <jovandjukic2@gmail.com>
2023-10-30 00:14:40 +0100

    test: sqlite connection test

---
+++ lib/database/sqlite/sqlite-connection.test.ts
@@ -0,0 +1,43 @@
+import { open } from "sqlite";
+import sqlite3 from "sqlite3";
+import { describe, expect, it, vi } from "vitest";
+import { SqliteConnection } from "./sqlite-connection.js";
+
+describe("SqliteDatabase", () => {
+  it("finds all rows", async () => {
+    const db = await open({ driver: sqlite3.Database, filename: ":memory:" });
+    const connection = new SqliteConnection(db);
+
+    await db.run("CREATE TABLE users (id INTEGER);");
+    await db.run("INSERT INTO users VALUES (1)");
+
+    const users = await connection.all("SELECT * FROM users;", []);
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(users).toStrictEqual([{ id: 1 }]);
+  });
+
+  it("finds single row", async () => {
+    const db = await open({ driver: sqlite3.Database, filename: ":memory:" });
+    const connection = new SqliteConnection(db);
+
+    await db.run("CREATE TABLE users (id INTEGER);");
+    await db.run("INSERT INTO users VALUES (1)");
+
+    const users = await connection.get("SELECT * FROM users;");
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(users).toStrictEqual({ id: 1 });
+  });
+
+  it("closes database", async () => {
+    const db = await open({ driver: sqlite3.Database, filename: ":memory:" });
+    const connection = new SqliteConnection(db);
+
+    db.close = vi.fn();
+
+    await connection.close();
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(db.close).toBeCalled();
+  });
+});
