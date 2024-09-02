b76310c1dbb4ceff0f7b49fd34248568322c9ad9
Jovan <jovandjukic2@gmail.com>
2024-09-02 15:46:55 +0200

    update

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 import { Application } from "@scream.js/http/application.interface";
-import { createExpressApp } from "@scream.js/http/express/create-express-application";
+import { createKoaApp } from "@scream.js/http/koa/create-koa-application";
 import { todoController } from "./src/todos";
 
-export const app: Application = createExpressApp();
+export const app: Application = createKoaApp();
 
 app
   .get("/", (ctx) =>
     ctx.json({
       name: "Jovan",
       message: "Rendered with nunjucks",
-    }),
+    })
   )
   .get("/about", (ctx) => ctx.render("about"));
 
