28fa08de575555fe6d247a33e0d7343ee0690e92
Jovan <jovandjukic2@gmail.com>
2024-12-08 13:54:55 +0100

    fix: repair app startup

--- src/todos/index.ts
+++ src/todos/index.ts
@@ -1,29 +1,10 @@
 import type { Repository } from "@scream.js/database/repository.js";
 
-import { open } from "sqlite";
-import sqlite3 from "sqlite3";
-
+import { db } from "config/database.js";
 import type { Todo } from "./todo.js";
 import { TodoRepository } from "./todo.repository.js";
 import { TodosController } from "./todos.controller.js";
 
-const db = await open({ driver: sqlite3.Database, filename: "db.sqlite" });
-
-const findTodoById = await db.prepare("SELECT * FROM todos WHERE id = ?;");
-const findAllTodos = await db.prepare("SELECT * FROM todos;");
-const insertTodo = await db.prepare(
-	"INSERT INTO todos(title, user_id) VALUES (?, ?);",
-);
-const updateTodo = await db.prepare("UPDATE todos SET title = ? WHERE id = ?;");
-const deleteTodo = await db.prepare("DELETE FROM todos WHERE id = ?");
-
-const todoRepository: Repository<Todo> = new TodoRepository(
-	db,
-	findTodoById,
-	findAllTodos,
-	insertTodo,
-	updateTodo,
-	deleteTodo,
-);
+const todoRepository: Repository<Todo> = new TodoRepository(db);
 
 export const todoController = new TodosController(todoRepository);
