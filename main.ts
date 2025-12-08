86c487c57c03672b1675aa9442eb5b6afaf082e4
Jovan <jovandjukic2@gmail.com>
2025-12-08 21:49:06 +0100

    refactor(app): inject logger into todo module and improve shutdown

--- main.ts
+++ main.ts
@@ -1,11 +1,23 @@
 import { createDB } from "@scream.js/database/db.js";
+import type { Application } from "@scream.js/http/application.js";
 import { createExpressApp } from "@scream.js/http/express/create-express-application.js";
+import type { Logger } from "@scream.js/logger/logger.interface.js";
+import { createLogger } from "@scream.js/logger/logger-factory.js";
+import type { Knex } from "knex";
 import { createTodoModule } from "./src/todos/index.js";
 
-export const createApp = () => {
-	const app = createExpressApp();
-	const db = createDB();
-	const { todoController } = createTodoModule(db);
+export type CreateAppOptions = Partial<{
+	app: Application;
+	db: Knex;
+	logger: Logger;
+}>;
+
+export const createApp = ({
+	app = createExpressApp(),
+	db = createDB(),
+	logger = createLogger(),
+}: CreateAppOptions = {}) => {
+	const { todoController } = createTodoModule(db, logger);
 
 	app.get("/", (ctx) =>
 		ctx.render("index", {
--- server.ts
+++ server.ts
@@ -2,6 +2,21 @@ import "source-map-support/register";
 import { logger } from "./config/logger.js";
 import { createApp } from "./main.js";
 
-createApp().app.listen(3000, () => {
+const { app, db } = createApp();
+
+const server = app.listen(3000, () => {
 	logger.log("Listening on port 3000");
 });
+
+const shutdown = async () => {
+	server.close(async (err) => {
+		if (err) {
+			throw err;
+		}
+
+		await db.destroy();
+	});
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
