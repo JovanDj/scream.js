1601b7a11ce61f2af6cb437aa94d27e6bfd52857
Jovan <jovandjukic2@gmail.com>
2026-03-22 16:06:33 +0100

    refactor(http): add ctx ref helper for aliased selects
    - expose knex ref through HttpContext implementations
    - replace string column aliases with ctx.ref(...).as(...) in project and todo controllers

--- lib/http/http-context.ts
+++ lib/http/http-context.ts
@@ -6,6 +6,7 @@ import type {
 
 export interface HttpContext {
 	db(table: string): ReturnType<Database>;
+	ref(column: string): ReturnType<Database["ref"]>;
 	transaction<T>(callback: (tx: DatabaseTransaction) => Promise<T>): Promise<T>;
 	param<T>(key: string, validator: Validator<T>): T | undefined;
 	body<T>(validator: Validator<T>): ValidationResult<T>;
